ccd51aa191dd58a20dac059d00a47cc2af1e56b7
MiniDan21 <molchanov_danilka00@mail.ru>
2023-12-10 23:46:24 +0300

    Saving before refactoring planarity.py

--- solver_tool/planarity.py
+++ solver_tool/planarity.py
@@ -18,32 +18,16 @@ class Planarity(Solver):
             elif self.define_K_5(graph):
                 planarited = False
                 break
-            for node in graph.define_minimal_adjects_count():
-                if len(node.adjects) == 0:
-                    graph._del_node(node)
-                else:
-                    graph.tight_edge(node, node._random_adject())
+
+            # ИСПРАВИТЬ КОСТЫЛЬ ДЛЯ ДВУДОЛЬНЫХ ГРАФОВ
+            node = graph.define_minimal_adjects_count()[-1]
+            if len(node.adjects) == 0:
+                graph._del_node(node)
+            else:                    
+                graph.tight_edge(node, node._random_adject())
 
         return planarited
 
-    # def graph_traversal(self, node: Node, directed: bool = False) -> None:
-    #     self.visited.append(node)
-    #     if directed:
-    #         if len(node.adjects.keys()) + len(node._reverse_adjects) > 2:
-    #             self.vertex_degree_gt_2.append(node)
-    #         if len(node.adjects.keys()) + len(node._reverse_adjects) > 3:
-    #             self.vertex_degree_gt_3.append(node)
-    #     else:
-    #         if len(node.adjects.keys()) > 2:
-    #             self.vertex_degree_gt_2.append(node)
-    #         if len(node.adjects.keys()) > 3:
-    #             self.vertex_degree_gt_3.append(node)
-        
-    #     for n in node.adjects:
-    #         if n not in self.visited:
-    #             self.graph_traversal(n, directed)
-
-        
     def define_K_3_3(self, graph: Graph) -> bool:
         if len(graph.info()) != 6:
             return False
